830d160bbfd7e4032cbfd551c85ed03e8ebf0751
Fourat ZOUARI <fourat@gmail.com>
2020-10-20 22:08:41 +0100

    Released a packaged txAMQP3 pypi version

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
 import os
 from setuptools import setup, find_packages
 
+
 def parse_requirements(filename):
     """load requirements from a pip requirements file"""
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and (not line.startswith("#") and not line.startswith('-'))]
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "txAMQP",
-    version = '0.9.0',
-    author = "Esteve Fernandez",
-    author_email = "esteve@apache.org",
-    description = "Python library for communicating with AMQP peers and brokers using Twisted",
-    license = 'Apache License 2.0',
-    packages = find_packages(exclude=["tests"]),
+    name="txAMQP3",
+    version='0.9.0',
+    author="Esteve Fernandez",
+    author_email="esteve@apache.org",
+    description="Python3 library for communicating with AMQP peers and brokers using Twisted",
+    license='Apache License 2.0',
+    packages=find_packages(exclude=["tests"]),
     # long_description=read('README.md'),
-    keywords = "twisted amq",
-    url = "https://github.com/txamqp/txamqp",
-    py_modules=["txAMQP"],
-    include_package_data = True,
-    package_data={'txamqp': ['README.md']},
-    install_requires = parse_requirements('requirements.txt'),
+    keywords="twisted amq",
+    url="https://github.com/jookies/txamqp",
+    py_modules=["txAMQP3"],
+    include_package_data=True,
+    package_data={'txamqp3': ['README.md']},
+    install_requires=parse_requirements('requirements.txt'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Twisted",
